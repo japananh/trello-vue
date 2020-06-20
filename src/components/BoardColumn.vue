df6fcc460d7e994792389e73d8be4def8024299d
na-seta <anhtn6797@seta-international.vn>
2020-06-20 13:51:11 +0700

    extracting drag and drop

--- src/components/BoardColumn.vue
+++ src/components/BoardColumn.vue
@@ -1,43 +1,46 @@
 <template>
-  <div
-    class="column"
-    draggable
-    @drop="moveTaskOrColumn($event, column.tasks, columnIndex)"
-    @dragover.prevent
-    @dragenter.prevent
-    @dragstart.self="pickupColumn($event, columnIndex)"
-  >
-    <div class="flex items-center mb-2 font-bold">
-      {{ column.name }}
-    </div>
-    <div class="list-reset">
-      <ColumnTask
-        v-for="(task, $taskIndex) of column.tasks"
-        :key="task.id"
-        :task="task"
-        :taskIndex="$taskIndex"
-        :column="column"
-        :columnIndex="columnIndex"
-        :board="board"
-      />
+  <AppDrop @drop="moveTaskOrColumn">
+    <AppDrag
+      class="column"
+      :transferData="{
+        type: 'column',
+        fromColumnIndex: columnIndex
+      }"
+    >
+      <div class="flex items-center mb-2 font-bold">
+        {{ column.name }}
+      </div>
+      <div class="list-reset">
+        <ColumnTask
+          v-for="(task, $taskIndex) of column.tasks"
+          :key="task.id"
+          :task="task"
+          :taskIndex="$taskIndex"
+          :column="column"
+          :columnIndex="columnIndex"
+          :board="board"
+        />
 
-      <input
-        type="text"
-        class="block p-2 w-full bg-transparent"
-        placeholder="+ Enter new task"
-        @keyup.enter="createTask($event, column.tasks)"
-      />
-    </div>
-  </div>
+        <input
+          type="text"
+          class="block p-2 w-full bg-transparent"
+          placeholder="+ Enter new task"
+          @keyup.enter="createTask($event, column.tasks)"
+        />
+      </div>
+    </AppDrag>
+  </AppDrop>
 </template>
 
 <script>
 import ColumnTask from "./ColumnTask";
 import movingTasksAndColumnsMixin from "@/mixins/movingTasksAndColumnsMixin";
+import AppDrag from "@/components/AppDrag";
+import AppDrop from "@/components/AppDrop";
 
 export default {
   mixins: [movingTasksAndColumnsMixin],
-  components: { ColumnTask },
+  components: { ColumnTask, AppDrag, AppDrop },
   props: {},
   methods: {
     pickupColumn(e, fromColumnIndex) {
--- src/components/ColumnTask.vue
+++ src/components/ColumnTask.vue
@@ -1,25 +1,30 @@
 <template>
-  <div
-    class="task"
-    draggable
-    @dragstart="pickupTask($event, taskIndex, columnIndex)"
-    @click="goToTask(task)"
-    @dragover.prevent
-    @dragenter.prevent
-    @drop.stop="moveTaskOrColumn($event, column.tasks, columnIndex, taskIndex)"
-  >
-    <span class="w-full flex-no-shrink font-bold">{{ task.name }}</span>
-    <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">
-      {{ task.description }}
-    </p>
-  </div>
+  <AppDrop @drop="moveTaskOrColumn">
+    <AppDrag
+      class="task"
+      :transferData="{
+        type: 'task',
+        fromColumnIndex: columnIndex,
+        fromTaskIndex: taskIndex
+      }"
+      @click="goToTask(task)"
+    >
+      <span class="w-full flex-no-shrink font-bold">{{ task.name }}</span>
+      <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">
+        {{ task.description }}
+      </p>
+    </AppDrag>
+  </AppDrop>
 </template>
 
 <script>
 import movingTasksAndColumnsMixin from "@/mixins/movingTasksAndColumnsMixin";
+import AppDrag from "@/components/AppDrag";
+import AppDrop from "@/components/AppDrop";
 
 export default {
   mixins: [movingTasksAndColumnsMixin],
+  components: { AppDrag, AppDrop },
   props: {
     taskIndex: {
       type: Number,
@@ -31,14 +36,6 @@ export default {
     }
   },
   methods: {
-    pickupTask(e, taskIndex, fromColumnIndex) {
-      e.dataTransfer.effectAllowed = "move";
-      e.dataTransfer.dropEffect = "move";
-
-      e.dataTransfer.setData("from-task-index", taskIndex);
-      e.dataTransfer.setData("from-column-index", fromColumnIndex);
-      e.dataTransfer.setData("type", "task");
-    },
     goToTask(task) {
       this.$router.push({ name: "task", params: { id: task.id } });
     }
